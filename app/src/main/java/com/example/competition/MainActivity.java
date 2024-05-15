658d0dc3a1811f3c799da90e8eb6d13e46e57dae
stepan <bohqdeweo@gmail.com>
2024-05-15 22:03:22 +0300

    test

--- app/src/main/java/com/example/competition/MainActivity.java
+++ app/src/main/java/com/example/competition/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int test_commit_var; //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
