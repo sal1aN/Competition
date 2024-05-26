ce6c1c0c19c07bf8d87c5b9bee71b824e5a99d86
sal1aN <popovicevgen1@gmail.com>
2024-05-26 22:21:52 +0300

    added sign up screen

--- app/src/main/java/com/example/competition/SignUpScreen.java
+++ app/src/main/java/com/example/competition/SignUpScreen.java
@@ -3,6 +3,9 @@ package com.example.competition;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +13,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SignUpScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Student");
+        arrayList.add("Teacher");
+        arrayList.add("Chief Teacher");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, arrayList);
+        spinner.setAdapter(adapter);
     }
 
-    public void toMainScreen(View view) {
-        Intent intent = new Intent(this, MainScreen.class);
+    public void ToSignInScreen(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void toSignUpScreen(View view) {
-        Intent intent = new Intent(this, SignUpScreen.class);
+    public void ToMainScreen(View view) {
+        Intent intent = new Intent(this, MainScreen.class);
         startActivity(intent);
     }
 }
